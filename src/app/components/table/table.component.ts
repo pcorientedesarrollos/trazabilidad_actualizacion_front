5140f876398a5261ec22b4b88221d5a2791fd21e
Paulina May <feripau230@gmail.com>
2025-08-06 21:24:36 -0600

    agregando tabla reusable

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-
+  @Input() columnas: { label: string, key: string , align : string}[] = [];
+  @Input() data: any[] = [];
+  @Input() templateAcciones?: TemplateRef<any>;
 }
+
--- src/app/pages/apicultores/apicultores.component.ts
+++ src/app/pages/apicultores/apicultores.component.ts
@@ -2,27 +2,34 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { TableComponent } from "../../components/table/table.component";
 
 @Component({
   selector: 'app-apicultores',
-  imports: [FooterComponent, CommonModule, SearchBarComponent],
+  imports: [FooterComponent, CommonModule, SearchBarComponent, TableComponent],
   templateUrl: './apicultores.component.html',
   styleUrl: './apicultores.component.css'
 })
 export class ApicultoresComponent {
- searchTerm = '';
-  apicultores = [
-  { nombre: 'Julio Martín Ku', acopiador: 'Valeria Gomez', apiarios: 15, colmenas: 45 },
-  { nombre: 'Fernando May', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
-  { nombre: 'Mariana Valenzuela', acopiador: 'Isabela Fuentes', apiarios: 15, colmenas: 45 },
-  { nombre: 'Sofía Villegas', acopiador: 'Santiago Delgado', apiarios: 15, colmenas: 45 },
-  { nombre: 'Leonel Rosado', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
-  { nombre: 'Elías Saens', acopiador: 'Isabela Fuentes', apiarios: 15, colmenas: 45 },
-  { nombre: 'Gloria Bacells', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
-  { nombre: 'Juliana Dominguez', acopiador: 'Valeria Gomez', apiarios: 15, colmenas: 45 },
+  searchTerm = '';
+columnas = [
+  { label: 'Nombre Apicultor', key: 'nombre', align: 'center' },
+  { label: 'Acopiador Asignado', key: 'acopiador', align: 'center' },
+  { label: 'N° Apiarios', key: 'apiarios', align: 'center' },
+  { label: 'N° Colmenas', key: 'colmenas', align: 'center' },
 ];
+  apicultores = [
+    { nombre: 'Julio Martín Ku', acopiador: 'Valeria Gomez', apiarios: 15, colmenas: 45 },
+    { nombre: 'Fernando May', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
+    { nombre: 'Mariana Valenzuela', acopiador: 'Isabela Fuentes', apiarios: 15, colmenas: 45 },
+    { nombre: 'Sofía Villegas', acopiador: 'Santiago Delgado', apiarios: 15, colmenas: 45 },
+    { nombre: 'Leonel Rosado', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
+    { nombre: 'Elías Saens', acopiador: 'Isabela Fuentes', apiarios: 15, colmenas: 45 },
+    { nombre: 'Gloria Bacells', acopiador: 'Emiliano Vargas', apiarios: 15, colmenas: 45 },
+    { nombre: 'Juliana Dominguez', acopiador: 'Valeria Gomez', apiarios: 15, colmenas: 45 },
+  ];
 
-onDownload() {
+  onDownload() {
     // Lógica para descargar el archivo
     console.log('Descargando archivo...');
   }
@@ -32,10 +39,22 @@ onDownload() {
     console.log('Agregando nuevo apicultor...');
   }
 
-  
-onSearchChange(term: string) {
-  this.searchTerm = term;
-  console.log('Buscando:', term);
-}
+  editar(apicultor: any) {
+    console.log('Editar', apicultor);
+  }
+
+  ver(apicultor: any) {
+    console.log('Ver', apicultor);
+  }
+
+  eliminar(apicultor: any) {
+    console.log('Eliminar', apicultor);
+  }
+
+
+  onSearchChange(term: string) {
+    this.searchTerm = term;
+    console.log('Buscando:', term);
+  }
 
 }
