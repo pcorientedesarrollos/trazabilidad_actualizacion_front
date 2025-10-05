9ad24824f5f14cb82f79b63ec497928e1f130818
PAULINA-MAY <feripau230@gmail.com>
2025-10-05 17:37:31 -0600

    agregando dashboard funcional

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -14,39 +14,22 @@ import { DashBoard } from './interface/dashboard.interface';
 export class DashboardComponent /* implements AfterViewInit */ {
 
     constructor(private dashBoardService: dashBoardService) { }
-/*   ngAfterViewInit(): void {
-    // Pie chart
-    new Chart("pieChart", {
-      type: 'pie',
-      data: {
-        labels: ["Work", "Eat", "Commute", "Watch TV", "Sleep"],
-        datasets: [{
-          data: [12, 8, 8, 8, 64],
-          backgroundColor: ["#1D4ED8", "#3B82F6", "#10B981", "#F59E0B", "#F97316"]
-        }]
-      }
-    }); 
-
-    // Bar chart
-     new Chart("barChart", {
-      type: 'bar',
-      data: {
-        labels: ["Copper", "Silver", "Gold", "Platinum"],
-        datasets: [{
-          label: "Electron Density",
-          backgroundColor: "#EF4444",
-          data: [8.9, 10.5, 19.3, 21.4]
-        }]
-      }
-    }); 
-  } */
-
+      dashboardData : any[] = [];
+      
+    ngOnInit(): void {
+    this.obtenerDashBoard();  
+  }
    obtenerDashBoard(){
     this.dashBoardService.getDashBoard().subscribe({
+      next:(data: DashBoard[]) =>{
+      this.dashboardData = data;
+      console.log('Este es mi data',data)
+    },
+    error: (err) => {
+    console.error('Error al obtener datos del dashboard:', err);
+  }
     })
-    next:(data: DashBoard[]) =>{
-      console.log(data)
-    }
+    
     
    }
 
